28ac944519d2a740572b96701b48f3e387c9a450
aulin1 <audrey.youran.lin@gmail.com>
2023-11-10 20:27:51 -0500

    made a new player

---
+++ player_controller.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_direction * speed
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
