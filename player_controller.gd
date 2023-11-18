1bfff2ff27eac585019b40b59c4bcf925e6a4d1d
Vi <haggertyv@icloud.com>
2023-11-17 20:00:08 -0500

    Revert "basic interaction (going to update to account for different items)"
    This reverts commit e5874f92e8322887dc423dd53acdaad1008fedfd.

--- player_controller.gd
+++ player_controller.gd
@@ -3,8 +3,6 @@ extends CharacterBody2D
 
 @export var speed = 400
 
-var interact = false # can the player interact with an item? (need to change to account for different items)
-
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
@@ -15,19 +13,6 @@ func get_input():
 		$AnimatedSprite2D.stop()
 
 
-func _process(delta):
-	if interact:
-		if Input.is_action_just_pressed("interact"):
-			print ("you interacted!!")
-
 func _physics_process(delta):
 	get_input()
 	move_and_collide(velocity * delta)
-
-
-func _on_area_2d_body_entered(_body):
-	interact = true
-
-
-func _on_area_2d_body_exited(_body):
-	interact = false
