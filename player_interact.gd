c3dba82921154768ea96c7a73d2f57af91facf64
Vi <haggertyv@icloud.com>
2023-11-17 20:11:32 -0500

    base interaction done (need to update to account for multiple objects)
    (ignore previous commits i was fighting with git)

--- player_interact.gd
+++ player_interact.gd
@@ -1,3 +1,16 @@
-extends CharacterBody2D
+extends Area2D
 
 #script for interaction here
+var interact = false #going to change this to account for different objects next
+
+func _process(_delta):
+	if interact:
+		if Input.is_action_just_pressed("interact"):
+			print("you just interacted!!")
+
+func _on_body_entered(_body):
+	interact = true
+
+
+func _on_body_exited(_body):
+	interact = false
