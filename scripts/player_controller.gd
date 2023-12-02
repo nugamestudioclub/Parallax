323b6c13ba4b8e1aee5062764f3cbda182e81234
aulin1 <audrey.youran.lin@gmail.com>
2023-12-01 19:22:17 -0500

    Added Mirror and Dresser
    Prevented player from exiting the screen

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 
 @export var speed = 400
+var screensize
+
+func _ready():
+	screensize = get_viewport_rect().size
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -11,6 +15,8 @@ func get_input():
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
 
 
 func _physics_process(delta):
