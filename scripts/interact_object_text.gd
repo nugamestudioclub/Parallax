1a2308706c9d5c5d6497a5ec7b6ac631b7cca746
Vi <haggertyv@icloud.com>
2023-12-01 19:50:03 -0500

    Added text for the mirror, dresser, and nightstand

--- scripts/interact_object_text.gd
+++ scripts/interact_object_text.gd
@@ -2,5 +2,5 @@ extends Node
 @export var object_text: String
 
 func on_interact():
-	print(object_text)
+	print(object_text) # this will be changed to an actual textbox whenever that is implemented
 
