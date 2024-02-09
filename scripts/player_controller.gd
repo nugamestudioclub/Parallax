ec758797eb89fa0c455e6184fbc66fcfb659ff4c
Sharyq Siddiqi <62477104+RyqShaw@users.noreply.github.com>
2024-02-09 18:49:14 -0500

    Updated Player Movement System, Added Main for Testing,  added Basic Sprite Handler(To be changed)

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,24 +1,42 @@
 extends CharacterBody2D
 
-
-@export var speed = 400
+@export var ACCEL: float  = 1600
+@export var MAX_SPEED: float  = 400
+@export var FRIC: float  = 1600
+#@export var speed = 400
 var screensize
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
 	screensize = get_viewport_rect().size
 
-func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_direction * speed
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
+func _physics_process(delta: float) -> void:
+	input_handler(delta)
+	move_and_slide()
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
-
-
-func _physics_process(delta):
-	get_input()
-	move_and_collide(velocity * delta)
+	
+func input_handler(delta: float) -> void:
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	# Sprite Handling
+	if input_direction.x < 0:
+		sprite.flip_h = input_direction.x < 0
+	elif input_direction.x > 0:
+		sprite.flip_h = false
+		
+	# Movement Handling
+	if input_direction != Vector2.ZERO:
+		velocity = velocity.move_toward(MAX_SPEED * input_direction, ACCEL*delta)
+		sprite.play()
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRIC*delta)
+		sprite.stop()
+	
+	velocity.normalized()
+#
+#
+#func _physics_process(delta):
+	#get_input()
+	#move_and_collide(velocity * delta)
